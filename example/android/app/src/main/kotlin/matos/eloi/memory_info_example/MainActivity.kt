1be5fbffa6598b6311f9ccb9e34cebcf472ecfac
AbsonDutraGalvao1 <absongalvao@gmail.com>
2024-10-14 15:49:10 -0300

    fix: error in version gradle

---
+++ example/android/app/src/main/kotlin/matos/eloi/memory_info_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package matos.eloi.memory_info_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/memory_info_usage.dart
@@ -0,0 +1,6 @@
+class MemoryInfoUsage {
+  final String availableMemory;
+  final String totalMemory;
+
+  MemoryInfoUsage(this.availableMemory, this.totalMemory);
+}
